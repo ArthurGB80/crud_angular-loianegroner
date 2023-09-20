0ec5fb76710a41bd1e7847cc083def8f8fa53a54
arthurgb80 <arthurgularte80@gmail.com>
2023-09-20 17:02:23 +0300

    Aula 4 - aula 7

---
+++ src/app/courses/courses/courses.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+import { Course } from '../model/course';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrls: ['./courses.component.scss'],
+})
+export class CoursesComponent {
+  courses: Course[] = [
+    { _id: '1', name: 'Angular', category: 'front-end'}
+  ];
+  displayedColumns = ['name', 'category'];
+
+  constructor() {
+    // this.courses = [];
+  }
+
+  ngOnInit(): void {}
+}
