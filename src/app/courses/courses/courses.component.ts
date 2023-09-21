7506d0f473e118d7567fb5ce2ee038ba18bdc43a
arthurgb80 <arthurgularte80@gmail.com>
2023-09-21 17:52:17 +0300

    Aula 8 - Aula 11

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,21 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { catchError, of, pipe } from 'rxjs';
+import { Observable } from 'rxjs/internal/Observable';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 
 import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
+
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss'],
+  providers: [CoursesService],
 })
-export class CoursesComponent {
-  courses: Course[] = [
-    { _id: '1', name: 'Angular', category: 'front-end'}
-  ];
+export class CoursesComponent implements OnInit {
+  courses$: Observable<Course[]>;
   displayedColumns = ['name', 'category'];
 
-  constructor() {
+  // coursesService: CoursesService
+
+  constructor(
+    private coursesService: CoursesService,
+    public dialog: MatDialog
+    ) {
     // this.courses = [];
+    //this.coursesService = new CoursesService();
+    
+    this.courses$ = this.coursesService.list();
+    pipe(
+      catchError((error) => {
+        this.onError('Erro ao carregar cursos.')
+        return of([]);
+      })
+    );
   }
+
+  onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: { errorMsg
+      }
+    });
+  }
+
 
   ngOnInit(): void {}
 }
+
+
---
+++ src/app/courses/services/courses.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { delay, first, tap } from 'rxjs';
+
+import { Course } from '../model/course';
+
+@Injectable()
+export class CoursesService {
+
+  private readonly API = '/assets/courses.json';
+
+  constructor(private httpClient: HttpClient) { }
+
+  list() {
+    return this.httpClient.get<Course[]>(this.API)
+    .pipe(
+      first(),
+      delay(5000),
+      tap(courses => console.log(courses))
+    );
+  }
+}
